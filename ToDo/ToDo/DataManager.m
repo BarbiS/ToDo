4f6c6c27d4e189312333c5b0183b8e7f88893a6a
Barbara Strugarevic <barbara.strugarevic@gmail.com>
2016-05-16 16:00:43 +0200

    WebViewController nastavak ...

--- ToDo/ToDo/DataManager.m
+++ ToDo/ToDo/DataManager.m
@@ -90,17 +90,60 @@
     }
 
 - (void)deleteObjectInDatabase:(NSManagedObject *)object {
+    [self.managedObjectContext deleteObject:object];
+    [self saveToDatabase];
 }
 - (void)updateObject:(NSManagedObject *)object {
+    NSError *error = nil;
+    
+    if ([object.managedObjectContext hasChanges] && ![object.managedObjectContext save:&error]) {
+        NSLog(@"Error updating object in database: %@, %@", [error localizedDescription], [error userInfo]);
+        abort();
     }
-- (void)logObject:(NSManagedObject *)object {
-        }
-- (CGFloat)numberOfTasksPerTaskGroup:(TaskGroup)group {
-    return 0.0;
 }
+- (void)logObject:(NSManagedObject *)object {
+    NSEntityDescription *description = [object entity];
+    NSDictionary *attributes = [description attributesByName];
+    
+    for (NSString *attribute in attributes) {
+        NSLog(@"%@ = %@,", attribute, [object valueForKey:attribute]);
+    }
+}
+- (CGFloat)numberOfTasksPerTaskGroup:(TaskGroup)group {
+    NSArray *tasksArray = [self fetchEntity:NSStringFromClass ([Task class])
+                           withFilter:[NSString stringWithFormat:@"group = %ld", group]
+                                withSortAsc:NO
+                                     forKey:nil];
+    return tasksArray.count;
+}
+
 - (void)saveTaskWithTitle:(NSString *)title
               description:(NSString *)description
                     group:(NSInteger)group {
+    Task *task = (Task *) [NSEntityDescription insertNewObjectForEntityForName:NSStringFromClass([Task class])
+                                                        inManagedObjectContext:self.managedObjectContext];
+    task.heading = title;
+    task.desc = description;
+    
+    if (self.userLocation) {
+        task.latitude = [NSNumber numberWithFloat:self.userLocation.coordinate.latitude];
+        task.longitude = [NSNumber numberWithFloat:self.userLocation.coordinate.longitude];
+    }
+    task.date = [NSDate date];
+    task.group = [NSNumber numberWithInteger:group];
+    
+    [self saveToDatabase];
+}
+
+#pragma mark - Private API
+
+-(void)saveToDatabase {
+    NSError *error = nil;
+    
+    if ([self.managedObjectContext hasChanges] && ![self.managedObjectContext save:&error]) {
+        NSLog(@"Unresolved error %@, %@", [error localizedDescription], [error userInfo]);
+        abort();
+    }
 }
 
 @end
